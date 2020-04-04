c2e669b63ca3fed4873fba0c3ba576d41d3a44a8
ellenliao66 <ellenliao66@163.com>
2020-04-04 16:40:21 +0800

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-04-04 16:32:03
+ * @LastEditTime: 2020-04-04 16:33:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \supermall\src\components\test.vue
+ -->
+<template>
+<div>
+  <h2>hello world</h2>
+</div>
+  
+</template>
+
+<script>
+export default {
+ name: 'test'
+}
+</script>
+
+<style>
+
+</style>
